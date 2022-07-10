b1e16561551f29c5ebbed61a7f95b9552d04eb43
alex <alexhong240@gmail.com>
2022-07-10 09:01:38 -0400

    dynamic game board

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -2,35 +2,46 @@ package tictactoe;
 
 public class TicTacToe {
 	
-	
-	public static char[][] board(int rows, int columns) {
-		
+	public static char[][] board(int rows, int columns) {	
 		char[][] board = new char[rows][columns];
 		for (int i =0; i < rows; i ++) {
 			for (int j = 0; j < columns; j++) {
 				board[i][j] = '1';	
 			}	
-		}	
+		}			
 			return board;
 	}
 	
-	
 	public static void printBoard(char[][] board) {
+		String line1 = "";
+		for (int k = 0; k < board[0].length + board[0].length-1; k ++) {
+			line1 += "-";
+		}
+		System.out.println(line1);
 		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {			
+			for (int j = 0; j < board[0].length; j++) {			
 				System.out.print(board[i][j]);
 				if (j < board[0].length-1) {
 					System.out.print("|");
-				}
+				}			
 			}
-			if ( i < board.length -1) {
-				
+			String line = "";
+			for (int k = 0; k < board[0].length + board[0].length-1; k ++) {
+				line += "-";
 			}
-			}	
+			System.out.println("\n" + line);		
+	}
+	}
+	
+	public static char[][] move(char[][] board, int row, int column, char symbol) {
+		board[row][column] = symbol;
+		return board;
 	}
 	
 	
 	public static void main(String[] args) {
-		printBoard(board(5, 5));
+		char[][] theBoard = board(3, 3);
+		move(theBoard, 0, 0, 'a');
+		printBoard(theBoard);
 	}
 }
